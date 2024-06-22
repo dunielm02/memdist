f0fc7433d6991d02fad9c3e2178783688a843972
Duniel Mesa <dunielm02@gmail.com>
2024-06-22 17:58:49 -0400

    added membership with serf

--- go.mod
+++ go.mod
@@ -10,8 +10,22 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.3 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/memberlist v0.5.0 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ internal/discovery/membership.go
@@ -0,0 +1,131 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+type SerfHandler interface {
+	Join(name string, addrs string) error
+	Leave(name string) error
+}
+
+type Membership struct {
+	Config
+	handler SerfHandler
+	serf    *serf.Serf
+	EventCh chan serf.Event
+	logger  *zap.Logger
+}
+
+func New(cfg Config, handler SerfHandler) (*Membership, error) {
+	m := &Membership{
+		Config:  cfg,
+		handler: handler,
+		logger:  zap.L(),
+	}
+
+	err := m.setUpRaft()
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+type Config struct {
+	NodeName       string
+	BindAddrs      string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+func (m *Membership) setUpRaft() error {
+	addrs, err := net.ResolveTCPAddr("tcp", m.BindAddrs)
+	if err != nil {
+		return err
+	}
+
+	config := serf.DefaultConfig()
+	config.Init()
+
+	config.MemberlistConfig.BindAddr = addrs.IP.String()
+	config.MemberlistConfig.BindPort = addrs.Port
+	m.EventCh = make(chan serf.Event)
+	config.EventCh = m.EventCh
+	config.Tags = m.Tags
+
+	config.NodeName = m.NodeName
+	m.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		_, err = m.serf.Join(m.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *Membership) eventHandler() {
+	for {
+		event := <-m.EventCh
+		switch event.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range event.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
+				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range event.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+func (m *Membership) handleJoin(member serf.Member) {
+	err := m.handler.Join(
+		member.Name,
+		member.Tags["rpc_addr"],
+	)
+	if err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(
+		member.Name,
+	); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	log := m.logger.Error
+	// if err == raft.ErrNotLeader {
+	// 	log = m.logger.Debug
+	// }
+	log(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rpc_addr", member.Tags["rpc_addr"]),
+	)
+}
